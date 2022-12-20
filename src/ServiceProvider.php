089240c99380e7202d2f5bfab1356a964331c7b0
Danil Lytvyn <daniilly@softwareplanetgroup.com>
2022-12-20 15:15:32 +0200

    DP-460 Upgrade to PHP 8.1 / Laravel 9

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -74,13 +74,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     protected function addMiddleware()
     {
-        // the method name was changed in Laravel 5.4
-        if (method_exists(Router::class, 'aliasMiddleware')) {
-            Route::aliasMiddleware('df.evaluate_limits', EvaluateLimits::class);
-        } else {
-            /** @noinspection PhpUndefinedMethodInspection */
-            Route::middleware('df.evaluate_limits', EvaluateLimits::class);
-        }
+        Route::aliasMiddleware('df.evaluate_limits', EvaluateLimits::class);
         Route::pushMiddlewareToGroup('df.api', 'df.evaluate_limits');
     }
 }
